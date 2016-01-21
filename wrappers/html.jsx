4c28f621857c618cb6b9c1685491fcdff03b38d3
Scott Nonnenberg <scott@nonnenberg.com>
2016-01-21 13:35:43 -0800

    Fix HTML wrapper, even if it's currently unused

--- wrappers/html.jsx
+++ wrappers/html.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {PropTypes} from 'react'
 
 module.exports = React.createClass({
-  render: function() {
-    var html;
-    html = "<div>fix me</div>";
+  propTypes: {
+    page: PropTypes.shape({
+      data: PropTypes.string,
+    }),
+  },
+
+  render: function () {
+    const html = this.props.page.data;
+
     return (
       <div dangerouslySetInnerHTML={{__html: html}}/>
     );
