74f91964991a598b78542618e90940c914542456
Scott Nonnenberg <scott@nonnenberg.com>
2016-01-22 15:34:39 -0800

    First version of popular.jsx
    (still need to figure out how to add it to the routes)

---
+++ pages/popular.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { RouteHandler, Link } from 'react-router';
+import sortBy from 'lodash/collection/sortBy';
+import DocumentTitle from 'react-document-title';
+import { link } from 'gatsby-helpers';
+import { rhythm, fontSizeToMS } from 'utils/typography'
+
+export default class extends React.Component {
+  render() {
+    let pages = filter(this.props.pages, function(page) {
+      const data = page.data;
+      return data && Boolean(page.data.hits);
+    })
+    pages = sortBy(pages, page => page.data.hits);
+
+    const pageLinks = map(pages, page => (
+      <li
+        key={page.path}
+        style={{
+          marginBottom: rhythm(1/4)
+        }}
+      >
+        <Link to={link(page.path)}>{page.data.title || page.path}</Link>
+      </li>
+    ));
+
+    return (
+      <DocumentTitle title={'Popular Posts | ' + this.props.config.blogTitle}>
+        <div>
+          <p
+            style={{
+              marginBottom: rhythm(2.5)
+            }}
+          >
+            <img
+              src="//www.gravatar.com/avatar/6d4e229c0d24e92a2d15499acab531d8?d=404"
+              style={{
+                float: 'left',
+                marginRight: rhythm(1/4),
+                marginBottom: 0,
+                width: rhythm(2),
+                height: rhythm(2)
+              }}
+            />
+            Written by <strong>{this.props.config.authorName}</strong> who lives and works in Seattle building useful things. <a href="https://twitter.com/scottnonnenberg">You should follow him on Twitter</a>
+          </p>
+          <ul>
+            {pageLinks}
+          </ul>
+        </div>
+      </DocumentTitle>
+    )
+  }
+}
