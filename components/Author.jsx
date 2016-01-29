1c0398d1c8e6b04f85dc1483219db2533cde9dff
Scott Nonnenberg <scott@nonnenberg.com>
2016-01-28 16:02:08 -0800

    Move all author info into config file

--- components/Author.jsx
+++ components/Author.jsx
@@ -6,12 +6,18 @@ import { link } from 'gatsby-helpers';
 
 import { rhythm, fontSizeToMS } from 'utils/typography'
 
+
 export default class Author extends React.Component {
   render() {
+    const config = this.props.config;
+    const image = config.authorImage;
+    const url = config.authorURL;
+    const blurb = config.authorBlurb;
+
     return (
       <div className="author">
-        <a href="https://scottnonnenberg.com"><img
-          src="//www.gravatar.com/avatar/6d4e229c0d24e92a2d15499acab531d8?d=404"
+        <a href={url}><img
+          src={image}
           style={{
             float: 'left',
             marginRight: rhythm(1/4),
@@ -20,10 +26,7 @@ export default class Author extends React.Component {
             height: rhythm(2)
           }}
         /></a>
-        Hi, I'm <a href="https://scottnonnenberg.com"><strong>{this.props.config.authorShortName}</strong></a>.
-        I'm a <a href="http://calpoly.edu">Computer Science major</a> turned <a href="http://visualstudio.com">Visual Studio</a> program
-        manager, now consultant and entrepreneur. <a href="mailto:scott@nonnenberg.com">Contact me</a> if
-        you need some training, coding, or just an experienced outside perspective!
+        <div dangerouslySetInnerHTML={{__html: blurb}} />
       </div>
     );
   }
