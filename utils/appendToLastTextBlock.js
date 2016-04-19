22ed6a034b25f1c23379e05296c26e76cad884f2
Scott Nonnenberg <scott@nonnenberg.com>
2016-04-19 12:29:29 -0700

    clean up appendToLastTextBlock
    thought I was going to share it as a code sample, but didn't. :0)

--- utils/appendToLastTextBlock.js
+++ utils/appendToLastTextBlock.js
@@ -5,14 +5,14 @@ var endOfBlockTest = /<\/p>/m;
 var endOfBlock = '</p>';
 
 export default function appendToLastTextBlock(content, toInsert) {
-  const blocks = filter(content.split(startOfBlock), function(block) {
-    return Boolean(block.length);
-  });
+  const blocks = filter(content.split(startOfBlock), (block) => Boolean(block.length));
 
-  for (var i = blocks.length - 1; i >= 0; i--) {
+  for (let i = blocks.length - 1; i >= 0; i--) {
     const block = blocks[i];
+
     const withoutTags = block.replace(/<[^>]*>/g, '');
     const withoutWhitespace = withoutTags.replace(/\s/g, '');
+
     if (withoutWhitespace.length) {
       blocks[i] = block.replace(endOfBlockTest, toInsert + endOfBlock);
       break;
