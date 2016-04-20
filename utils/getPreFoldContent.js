1c6f13b2218fb18386a0049073ff931e5a3aa9b2
Scott Nonnenberg <scott@nonnenberg.com>
2016-04-20 14:16:05 -0700

    Eliminate extra space between title/content

--- utils/getPreFoldContent.js
+++ utils/getPreFoldContent.js
@@ -1,8 +1,9 @@
-var prefix = /<p>(<em>)?\[[^\]]+\](<\/em>)?<\/p>/m;
+var prefix = /<p>(<em>)?\[[^\]]+\](<\/em>)?<\/p>\n/m;
 var fold = /<div class='fold'/;
 
 export default function getPreFoldContent(content) {
   const foldMatch = fold.exec(content);
+
   if (foldMatch) {
     content = content.slice(0, foldMatch.index);
   }
