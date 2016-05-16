55e95c68b813186c9a152eb273781a7b1381d997
Scott Nonnenberg <scott@nonnenberg.com>
2016-05-16 09:26:35 -0700

    Re-add imports/first rule

--- scripts/.eslintrc.js
+++ scripts/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   rules: {
     'no-sync': 'off',
 
-    // 'import/imports-first': 'off',
+    'import/imports-first': 'off',
     'import/no-extraneous-dependencies': 'off',
 
     'security/detect-non-literal-fs-filename': 'off',
