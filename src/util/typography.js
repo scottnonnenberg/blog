1cf3344e43f753429a5871b6bdd592c963429470
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-05 21:59:22 -0700

    chore(deps): update typography.js

--- src/util/typography.js
+++ src/util/typography.js
@@ -3,9 +3,19 @@ import Typography from 'typography';
 const options = {
   baseFontSize: '18px',
   baseLineHeight: '31px',
-  modularScales: [{
-    scale: 'major third',
-  }],
+  modularScales: [
+    {
+      scale: 2.35,
+    },
+  ],
+  headerFontFamily: [
+    'Avenir Next',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
 };
 
 const typography = new Typography(options);
